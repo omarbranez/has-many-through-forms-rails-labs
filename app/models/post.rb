bfc1973a92ad297ea04ec9d3391273d8b049680b
Omar <omar.branez@gmail.com>
2021-06-19 14:36:00 -0400

    commit tree 22d28b0bfe547f15cd43c239aff6339e1abdfe34

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,16 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories
 
-
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      if category_attribute["name"].present?
+        category = Category.find_or_create_by(category_attribute)
+        # allows new post form to create a post without a new category, as long as it has a name
+        self.categories << category
+        # category ids is an ARRAY
+      end
+    end
+  end
 end
